665af4195cca54a3966bbdd0e376a3980dadfe75
DDDrop <wylroro.dev@gmail.com>
2021-05-28 19:08:17 +0900

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -14,29 +16,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+    if (Platform.isIOS) {
+      FlutterApplovin.init("2a4cf642c3f05167");
+    } else if (Platform.isAndroid) {
+      FlutterApplovin.init("9a5c7a46428fdd68");
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    super.initState();
   }
+
+  void listener(AppLovionStatus status, AppLovinAdInfo info) {
+    print(status);
+    if (status == AppLovionStatus.didRewardUser) {
+      print('👍get reward');
+    }
+  }
+
+  bool isRewardedVideoAvailable = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +43,15 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: OutlinedButton(
+            onPressed: () async {
+              isRewardedVideoAvailable = await FlutterApplovin.isLoaded();
+              if (isRewardedVideoAvailable) {
+                FlutterApplovin.showRewordVideo(listener);
+              }
+            },
+            child: Text('Show Reward Video'),
+          ),
         ),
       ),
     );
