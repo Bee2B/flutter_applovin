f7804d9b7a092aef580c464caf5ee7e7985489a2
DDDrop <wylroro.dev@gmail.com>
2021-05-29 12:07:48 +0900

    Add implement for android

--- android/src/main/kotlin/com/example/flutter_applovin/FlutterApplovinPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_applovin/FlutterApplovinPlugin.kt
@@ -1,5 +1,7 @@
 package com.example.flutter_applovin
 
+import android.app.Activity
+import android.content.Context
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -7,29 +9,80 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
+import java.lang.Exception
+import com.applovin.sdk.AppLovinMediationProvider;
+import com.applovin.sdk.AppLovinSdk;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
 /** FlutterApplovinPlugin */
-class FlutterApplovinPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterApplovinPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private lateinit var rewardInstance: RewardedVideo
+    lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_applovin")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_applovin")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "init" -> {
+                try {
+                    val args = call.arguments as Map<*, *>
+                    val unitId = args["unitId"].toString()
+                    appLovinInit(unitId, result)
+                } catch (e: Exception) {
+                    //
+                }
+            }
+            "showRewardVideo" -> {
+                rewardInstance.show()
+                result.success(true)
+            }
+            "isLoaded" -> {
+                result.success(rewardInstance.isLoaded())
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    private fun appLovinInit(unitId: String, @NonNull result: Result) {
+        AppLovinSdk.getInstance(context).mediationProvider = AppLovinMediationProvider.MAX
+        rewardInstance = RewardedVideo(unitId, this)
+        result.success(true)
+    }
+
+    fun callback(method: String, arguments: Map<String, *>) {
+        this.activity.runOnUiThread( Runnable {
+            kotlin.run {
+                this.channel.invokeMethod(method, arguments)
+            }
+        })
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+    }
 }
---
+++ android/src/main/kotlin/com/example/flutter_applovin/RewardedVideo.kt
@@ -0,0 +1,92 @@
+package com.example.flutter_applovin
+
+import com.applovin.mediation.*
+import com.applovin.mediation.ads.MaxRewardedAd;
+import java.lang.Exception
+import java.util.*
+
+class RewardedVideo(unitId: String, private var plugin: FlutterApplovinPlugin) : MaxRewardedAdListener {
+    private var rewardedAd: MaxRewardedAd = MaxRewardedAd.getInstance(unitId, plugin.activity)
+    private var retryAttempt: Int = 0
+
+    init {
+        rewardedAd.setListener(this)
+        rewardedAd.loadAd()
+    }
+
+    fun show() {
+        try {
+            if (rewardedAd.isReady) {
+                rewardedAd.showAd()
+            }
+        } catch (e: Exception) {
+            //
+        }
+    }
+
+    fun isLoaded(): Boolean {
+        return rewardedAd.isReady
+    }
+
+
+    override fun onAdLoaded(ad: MaxAd?) {
+        retryAttempt = 0
+        plugin.callback("onAdLoaded", mapMAAd(ad))
+    }
+
+    override fun onAdDisplayed(ad: MaxAd?) {
+        plugin.callback("didDisplay", mapMAAd(ad))
+    }
+
+    override fun onAdHidden(ad: MaxAd?) {
+        plugin.callback("didHide", mapMAAd(ad))
+        rewardedAd.loadAd()
+    }
+
+    override fun onAdClicked(ad: MaxAd?) {
+        plugin.callback("didClick", mapMAAd(ad))
+    }
+
+    override fun onAdRevenuePaid(ad: MaxAd?) {
+        plugin.callback("didPayRevenue", mapMAAd(ad))
+    }
+
+    override fun onAdLoadFailed(adUnitId: String?, error: MaxError?) {
+        plugin.callback("didFailToLoadAd", mapOf<String, Any>())
+    }
+
+    override fun onAdDisplayFailed(ad: MaxAd?, error: MaxError?) {
+        plugin.callback("didFailToDisplay", mapMAAd(ad))
+    }
+
+    override fun onRewardedVideoStarted(ad: MaxAd?) {
+        plugin.callback("didStartRewardedVideo", mapMAAd(ad))
+    }
+
+    override fun onRewardedVideoCompleted(ad: MaxAd?) {
+        plugin.callback("didCompleteRewardedVideo", mapMAAd(ad))
+    }
+
+    override fun onUserRewarded(ad: MaxAd?, reward: MaxReward?) {
+        plugin.callback("didRewardUser", mapMAAd(ad))
+    }
+
+    private fun convertMAAdFormatToString(format: MaxAdFormat): String {
+        return format.displayName.toLowerCase(Locale.ROOT)
+    }
+
+    private fun mapMAAd(ad: MaxAd?): Map<String, Any> {
+        if (ad == null) {
+            return mapOf()
+        } else {
+            return mapOf(
+                    Pair("format", convertMAAdFormatToString(ad.format)),
+                    Pair("adUnitIdentifier", ad.adUnitId),
+                    Pair("networkName", ad.networkName),
+                    Pair("creativeIdentifier", ad.creativeId),
+                    Pair("revenue", ad.revenue),
+                    Pair("placement", ad.placement)
+            )
+        }
+    }
+}
